6c066343edb5cc9c19cfe6a547fea1d57b26a461
Spencer Fevreau <spencer.fevreau@dcmail.ca>
2026-06-12 02:35:41 -0400

    Changed annotation in push.ts

--- web/src/lib/push.ts
+++ web/src/lib/push.ts
@@ -1,6 +1,6 @@
 import { api } from './api';
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): Uint8Array<ArrayBuffer> {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
   const raw = window.atob(base64);
